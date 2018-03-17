11c51224c359355a6dfc9e3ee0f1598ace45f11d
anythingtechpro <anythingtechpro@gmail.com>
2018-03-17 15:12:48 -0400

    eliminate multiple redefinitions of cryptonight types

--- cryptonight_dark.c
+++ cryptonight_dark.c
@@ -22,13 +22,10 @@
 // SOFTWARE.
 
 #include "crypto/oaes_lib.h"
-#include "crypto/c_keccak.h"
-#include "crypto/c_groestl.h"
-#include "crypto/c_blake256.h"
-#include "crypto/c_jh.h"
-#include "crypto/c_skein.h"
 #include "crypto/int-util.h"
 #include "crypto/hash-ops.h"
+
+#include "cryptonight_util.h"
 
 #define MEMORY         (1 << 19) /* 0.5 MiB */
 #define ITER           (1 << 18)
@@ -46,28 +43,6 @@ union cn_slow_hash_state {
     };
 };
 #pragma pack(pop)
-
-static void do_blake_hash(const void* input, size_t len, char* output) {
-    blake256_hash((uint8_t*)output, input, len);
-}
-
-void do_groestl_hash(const void* input, size_t len, char* output) {
-    groestl(input, len * 8, (uint8_t*)output);
-}
-
-static void do_jh_hash(const void* input, size_t len, char* output) {
-    int r = jh_hash(HASH_SIZE * 8, input, 8 * len, (uint8_t*)output);
-    assert(SUCCESS == r);
-}
-
-static void do_skein_hash(const void* input, size_t len, char* output) {
-    int r = c_skein_hash(8 * HASH_SIZE, input, 8 * len, (uint8_t*)output);
-    assert(SKEIN_SUCCESS == r);
-}
-
-static void (* const extra_hashes[4])(const void *, size_t, char *) = {
-    do_blake_hash, do_groestl_hash, do_jh_hash, do_skein_hash
-};
 
 extern int aesb_single_round(const uint8_t *in, uint8_t*out, const uint8_t *expandedKey);
 extern int aesb_pseudo_round(const uint8_t *in, uint8_t *out, const uint8_t *expandedKey);
---
+++ cryptonight_util.h
@@ -0,0 +1,47 @@
+// Copyright (c) 2012-2013 The Cryptonote developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+// Portions Copyright (c) 2018 The Monero developers
+
+#ifndef CRYPTONIGHT_UTIL_H
+#define CRYPTONIGHT_UTIL_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include <stdint.h>
+
+#include "crypto/c_keccak.h"
+#include "crypto/c_groestl.h"
+#include "crypto/c_blake256.h"
+#include "crypto/c_jh.h"
+#include "crypto/c_skein.h"
+
+static void do_blake_hash(const void* input, size_t len, char* output) {
+    blake256_hash((uint8_t*)output, input, len);
+}
+
+void do_groestl_hash(const void* input, size_t len, char* output) {
+    groestl(input, len * 8, (uint8_t*)output);
+}
+
+static void do_jh_hash(const void* input, size_t len, char* output) {
+    int r = jh_hash(HASH_SIZE * 8, input, 8 * len, (uint8_t*)output);
+    assert(SUCCESS == r);
+}
+
+static void do_skein_hash(const void* input, size_t len, char* output) {
+    int r = c_skein_hash(8 * HASH_SIZE, input, 8 * len, (uint8_t*)output);
+    assert(SKEIN_SUCCESS == r);
+}
+
+static void (* const extra_hashes[4])(const void *, size_t, char *) = {
+    do_blake_hash, do_groestl_hash, do_jh_hash, do_skein_hash
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
